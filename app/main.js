c9812f2b47e900d3405359405e90d30d9b6e4142
Jason Diamond <jason@diamond.name>
2015-04-12 17:43:49 -0700

    better example app including pub/sub

--- app/main.js
+++ app/main.js
@@ -1,27 +1,70 @@
-document.getElementById('send').onclick = function() {
-    var msg = document.getElementById('msg').value;
-
-    msg = JSON.parse(msg);
+(function() {
 
     var application = 'com.my_company.my_application';
-    var connect = true;
+    var port = null;
 
-    if (connect) {
-        var port = chrome.runtime.connectNative(application);
+    document.getElementById('connect').addEventListener('click', function() {
+        log('chrome.runtime.connectNative')
+
+        port = chrome.runtime.connectNative(application);
 
         port.onMessage.addListener(log);
 
-        port.onDisconnect.addListener(function() {
-            log({ event: 'disconnect' });
-        });
+        port.onDisconnect.addListener(function(e) {
+            log('unexpected disconnect');
 
-        port.postMessage(msg);
-    } else {
-        chrome.runtime.sendNativeMessage(application, msg, log);
-    }
+            port = null;
+        });
+    });
+
+    document.getElementById('disconnect').addEventListener('click', function() {
+        log('port.disconnect');
+        port.disconnect(); // this doesn't seem to trigger the onDisconnect event
+        port = null;
+    });
+
+    var examples = {
+        ping: { ping: 'pong' },
+        readdir: { readdir: '/' },
+        subscribe: { subscribe: 'time' },
+        unsubscribe: { unsubscribe: 'time' }
+    };
+
+    Array.prototype.slice.call(document.querySelectorAll('[data-example]')).forEach(function(example) {
+        example.addEventListener('click', function() {
+            document.getElementById('msg').value = JSON.stringify(examples[example.dataset.example]);
+        });
+    });
+
+    document.getElementById('send').addEventListener('click', function() {
+        var json = document.getElementById('msg').value;
+        var msg;
+
+        try {
+            msg = JSON.parse(json);
+        } catch (err) {
+            return log('invalid JSON: ' + json);
+        }
+
+        if (port) {
+            log('port.postMessage');
+            port.postMessage(msg);
+        } else {
+            log('chrome.runtime.sendNativeMessage');
+            chrome.runtime.sendNativeMessage(application, msg, log);
+        }
+    });
+
+    document.getElementById('clear').addEventListener('click', function() {
+        document.getElementById('log').innerHTML = '';
+    });
 
     function log(msg) {
         console.log(msg);
-        document.getElementById('response').innerText += JSON.stringify(msg) + '\n';
+
+        var e = document.createElement('pre');
+        e.appendChild(document.createTextNode(typeof msg === 'object' ? JSON.stringify(msg) : msg));
+        document.getElementById('log').appendChild(e);
     }
-};
+
+})();
--- host/my_host.js
+++ host/my_host.js
@@ -1,36 +1,59 @@
 #!/usr/local/bin/node
 
-// Best to use a direct path on the shebang line in case node isn't in the
-// PATH when launched by Chrome.
+// Might be good to use an explicit path to node on the shebang line in case
+// it isn't in PATH when launched by Chrome.
 
 var fs = require('fs');
 
 var nativeMessage = require('../index');
 
+var input = new nativeMessage.Input();
+var transform = new nativeMessage.Transform(messageHandler);
+var output = new nativeMessage.Output();
+
 process.stdin
-    .pipe(new nativeMessage.Input())
-    .pipe(new nativeMessage.Transform(function(msg, push, done) {
-        // If it has a readdir property...
-        if (msg && msg.readdir) {
-            // ...reply with a message for each file in that directory.
-            fs.readdir(msg.readdir, function(err, files) {
-                if (err) {
-                    push({ error: err.message || err });
-                } else {
-                    files.forEach(function(file) {
-                        push({ file: file });
-                    });
-                }
-
-                done();
-            });
-        } else {
-            // Just echo the message:
-            push(msg);
-
-            done();
-        }
-    }))
-    .pipe(new nativeMessage.Output())
+    .pipe(input)
+    .pipe(transform)
+    .pipe(output)
     .pipe(process.stdout)
 ;
+
+var subscriptions = {};
+
+var timer = setInterval(function() {
+    if (subscriptions.time) {
+        output.write({ time: new Date().toISOString() });
+    }
+}, 1000);
+
+input.on('end', function() {
+    clearInterval(timer);
+});
+
+function messageHandler(msg, push, done) {
+    if (msg.readdir) {
+        fs.readdir(msg.readdir, function(err, files) {
+            if (err) {
+                push({ error: err.message || err });
+            } else {
+                files.forEach(function(file) {
+                    push({ file: file });
+                });
+            }
+
+            done();
+        });
+    } else if (msg.subscribe) {
+        subscriptions[msg.subscribe] = true;
+        push({ subscribed: msg.subscribe });
+        done();
+    } else if (msg.unsubscribe) {
+        delete subscriptions[msg.unsubscribe];
+        push({ unsubscribed: msg.unsubscribe });
+        done();
+    } else {
+        // Just echo the message:
+        push(msg);
+        done();
+    }
+}
